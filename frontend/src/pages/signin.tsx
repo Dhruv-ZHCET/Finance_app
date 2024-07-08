cccd42f3f7ba8769a621dd5cec8debf74a692580
Dhruv <Dhruv262004@gmail.com>
2024-07-08 23:49:28 +0530

    final

--- frontend/src/pages/signin.tsx
+++ frontend/src/pages/signin.tsx
@@ -1,17 +1,15 @@
+import { Auth2 } from "../components/Auth_signin";
+import { Quote } from "../components/Quote";
+export const Signin = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 ">
+      <div className="col-span-1 h-screen flex items-center justify-center">
+        <Auth2 str="signin" />
+      </div>
 
-import { Auth2 } from "../components/Auth_signin"
-import { Quote } from "../components/Quote"
-export const Signin = () =>{
-    return(
-        <div className="grid grid-cols-1 lg:grid-cols-2 ">
-            <div className="col-span-1 flex items-center justify-center">
-                <Auth2 str="signin"/>
-            </div>
-            
-            <div className=" col-span-1 hidden lg:block">
-                <Quote />
-            </div>
-            
-        </div>
-        )
-}
+      <div className=" col-span-1 hidden lg:block">
+        <Quote />
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -1,16 +1,15 @@
-import { Quote } from "../components/Quote"
+import { Quote } from "../components/Quote";
 import { Auth } from "../components/Auth";
-export const Signup = ()=>{
-    return(
-        <div className="grid grid-cols-1 lg:grid-cols-2 ">
-            <div className="col-span-1 flex items-center justify-center">
-                <Auth  str="signup"/>
-            </div>
-            
-            <div className=" col-span-1 hidden lg:block">
-                <Quote />
-            </div>
-            
-        </div>
-    )
-}
+export const Signup = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 ">
+      <div className="col-span-1 h-screen flex items-center justify-center">
+        <Auth str="signup" />
+      </div>
+
+      <div className=" col-span-1 hidden lg:block">
+        <Quote />
+      </div>
+    </div>
+  );
+};
