e6a391c722020b445190db1a74bbeec19d6953aa
Dhruv <Dhruv262004@gmail.com>
2024-07-08 22:23:39 +0530

    finished yeahh!!!!

--- frontend/src/components/Row2.tsx
+++ frontend/src/components/Row2.tsx
@@ -1,15 +1,11 @@
-import axios from "axios";
 import { DashboardCard } from "./DashboardCard";
-import { error } from "console";
 import {
   ResponsiveContainer,
   CartesianGrid,
-  AreaChart,
   XAxis,
   YAxis,
   ZAxis,
   Tooltip,
-  Area,
   LineChart,
   Line,
   ScatterChart,
@@ -28,20 +24,23 @@ import FlexBetween from "./flexbetween";
 import { Box, Typography } from "@mui/material";
 
 const PieData = [
-  {"name": "Group A", "value":600},
-  {"name": "Group B", "value":400},
-]
+  { name: "Group A", value: 600 },
+  { name: "Group B", value: 400 },
+];
 
 export const Row2 = () => {
   const theme = useTheme();
-  const pieColors = [themeSettings.palette.primary[800],themeSettings.palette.primary[300]];
+  const pieColors = [
+    themeSettings.palette.primary[800],
+    themeSettings.palette.primary[300],
+  ];
 
   const [data, setData] = useState<
     { name: string; operational: number; nonOperational: number }[]
   >([]);
   const [data2, setData2] = useState<{ Price: number; Expense: number }[]>([]);
-  const [loading1,setloading1] = useState(true);
-  const [loading2,setloading2] = useState(true);
+  const [loading1, setloading1] = useState(true);
+  const [loading2, setloading2] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
       const response = await getKpi();
@@ -79,73 +78,77 @@ export const Row2 = () => {
   return (
     <>
       <DashboardCard gridArea="d">
-        {loading1?
-        <div className="flex justify-center items-center h-full">
-        <button
-        type="button"
-        className="pointer-events-none inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 disabled:opacity-70 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
-        disabled>
-        <div
-          className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-          role="status"></div>
-        <span>Loading...</span>
-      </button>
-      </div>:
-      <>
-        <BoxHeader
-          title="Operational v/s Non-operational Expenses"
-          extra="+4%"
-        />
-        <ResponsiveContainer width="100%" height="100%">
-          <LineChart
-            data={data}
-            margin={{
-              top: 0,
-              right: 0,
-              left: -10,
-              bottom: 55,
-            }}
-          >
-            <CartesianGrid
-              vertical={false}
-              stroke={themeSettings.palette.grey[800]}
+        {loading1 ? (
+          <div className="flex justify-center items-center h-full">
+            <button
+              type="button"
+              className="pointer-events-none inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 disabled:opacity-70 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+              disabled
+            >
+              <div
+                className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                role="status"
+              ></div>
+              <span>Loading...</span>
+            </button>
+          </div>
+        ) : (
+          <>
+            <BoxHeader
+              title="Operational v/s Non-operational Expenses"
+              extra="+4%"
             />
-            <XAxis dataKey="name" interval={0} fontSize={"10px"} />
-            <YAxis
-              yAxisId="left"
-              fontSize={"10px"}
-              tickLine={false}
-              axisLine={false}
-            />
-            <YAxis
-              yAxisId="right"
-              orientation="right"
-              fontSize={"10px"}
-              tickLine={false}
-              axisLine={false}
-              domain={[0, 11000]}
-            />
-            <Tooltip />
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart
+                data={data}
+                margin={{
+                  top: 0,
+                  right: 0,
+                  left: -10,
+                  bottom: 55,
+                }}
+              >
+                <CartesianGrid
+                  vertical={false}
+                  stroke={themeSettings.palette.grey[800]}
+                />
+                <XAxis dataKey="name" interval={0} fontSize={"10px"} />
+                <YAxis
+                  yAxisId="left"
+                  fontSize={"10px"}
+                  tickLine={false}
+                  axisLine={false}
+                />
+                <YAxis
+                  yAxisId="right"
+                  orientation="right"
+                  fontSize={"10px"}
+                  tickLine={false}
+                  axisLine={false}
+                  domain={[0, 11000]}
+                />
+                <Tooltip />
 
-            <Line
-              yAxisId="left"
-              type="monotone"
-              dataKey="operational"
-              stroke={themeSettings.palette.tertiary[500]}
-            />
-            <Line
-              yAxisId="right"
-              type="monotone"
-              dataKey="nonOperational"
-              stroke={themeSettings.palette.primary.main}
-            />
-          </LineChart>
-        </ResponsiveContainer>
-        </>}
+                <Line
+                  yAxisId="left"
+                  type="monotone"
+                  dataKey="operational"
+                  stroke={themeSettings.palette.tertiary[500]}
+                />
+                <Line
+                  yAxisId="right"
+                  type="monotone"
+                  dataKey="nonOperational"
+                  stroke={themeSettings.palette.primary.main}
+                />
+              </LineChart>
+            </ResponsiveContainer>
+          </>
+        )}
       </DashboardCard>
       <DashboardCard gridArea="e">
-      <BoxHeader title="Campaigns & Targets" extra="+4%"/>
-      <FlexBetween mt="0.25rem" gap="1.5rem">
+        <BoxHeader title="Campaigns & Targets" extra="+4%" />
+        <FlexBetween mt="0.25rem" gap="1.5rem">
           <PieChart
             width={110}
             height={100}
@@ -169,9 +172,9 @@ export const Row2 = () => {
               ))}
             </Pie>
           </PieChart>
-          <Box  flexBasis="40%" textAlign="center" mb="1rem" >
+          <Box flexBasis="40%" textAlign="center" mb="1rem">
             <Typography variant="h5">Target Sales</Typography>
-            <Typography  variant="h3" color={themeSettings.palette.primary[300]}>
+            <Typography variant="h3" color={themeSettings.palette.primary[300]}>
               83
             </Typography>
             <Typography variant="h6">
@@ -189,61 +192,67 @@ export const Row2 = () => {
             </Typography>
           </Box>
         </FlexBetween>
-      
       </DashboardCard>
       <DashboardCard gridArea="f">
-        {loading2?
-        <div className="flex justify-center items-center h-full">
-        <button
-        type="button"
-        className="pointer-events-none inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 disabled:opacity-70 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
-        disabled>
-        <div
-          className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-          role="status"></div>
-        <span>Loading...</span>
-      </button>
-      </div>:
-      <>
-        <BoxHeader title="Product Prices v/s Expenses" extra="+4%"></BoxHeader>
-        <ResponsiveContainer width="100%" height="100%">
-          <ScatterChart
-            margin={{
-              top: 0,
-              right: 25,
-              bottom: 40,
-              left: -10,
-            }}
-          >
-            <CartesianGrid stroke={themeSettings.palette.grey[800]} />
-            <XAxis
-              type="number"
-              dataKey="price"
-              tickLine={false}
-              axisLine={false}
-              style={{ fontSize: "10px" }}
-              tickFormatter={(v) => `$${v}`}
-              name="Price"
-            />
-            <YAxis
-              type="number"
-              dataKey="expense"
-              tickLine={false}
-              axisLine={false}
-              style={{ fontSize: "10px" }}
-              tickFormatter={(v) => `$${v}`}
-              name="Expense"
-            />
-            <ZAxis type="number" range={[20]} />
-            <Tooltip formatter={(v) => `$${v}`} />
-            <Scatter
-              name="Product Expense Ratio"
-              data={data2}
-              fill={themeSettings.palette.tertiary[500]}
-            />
-          </ScatterChart>
-        </ResponsiveContainer>
-        </>}
+        {loading2 ? (
+          <div className="flex justify-center items-center h-full">
+            <button
+              type="button"
+              className="pointer-events-none inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 disabled:opacity-70 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+              disabled
+            >
+              <div
+                className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                role="status"
+              ></div>
+              <span>Loading...</span>
+            </button>
+          </div>
+        ) : (
+          <>
+            <BoxHeader
+              title="Product Prices v/s Expenses"
+              extra="+4%"
+            ></BoxHeader>
+            <ResponsiveContainer width="100%" height="100%">
+              <ScatterChart
+                margin={{
+                  top: 0,
+                  right: 25,
+                  bottom: 40,
+                  left: -10,
+                }}
+              >
+                <CartesianGrid stroke={themeSettings.palette.grey[800]} />
+                <XAxis
+                  type="number"
+                  dataKey="price"
+                  tickLine={false}
+                  axisLine={false}
+                  style={{ fontSize: "10px" }}
+                  tickFormatter={(v) => `$${v}`}
+                  name="Price"
+                />
+                <YAxis
+                  type="number"
+                  dataKey="expense"
+                  tickLine={false}
+                  axisLine={false}
+                  style={{ fontSize: "10px" }}
+                  tickFormatter={(v) => `$${v}`}
+                  name="Expense"
+                />
+                <ZAxis type="number" range={[20]} />
+                <Tooltip formatter={(v) => `$${v}`} />
+                <Scatter
+                  name="Product Expense Ratio"
+                  data={data2}
+                  fill={themeSettings.palette.tertiary[500]}
+                />
+              </ScatterChart>
+            </ResponsiveContainer>
+          </>
+        )}
       </DashboardCard>
     </>
   );
--- frontend/src/expanded-theme.ts
+++ frontend/src/expanded-theme.ts
@@ -1,5 +1,4 @@
 //eslint-disable-next-line
-import { Palette, PaletteColor, TypeBackground } from "@mui/material/styles/createPalette";
 
 declare module "@mui/material/styles/createPalette" {
   interface PaletteColor {
